fdb71b7c7e728f81f5cfec2e919ad5a4e4967f3f
Alexey Vedushev <alexey.vedushev@omega-r.com>
2021-04-03 01:55:55 +0300

    commit

---
+++ Vertex.swift
@@ -0,0 +1,16 @@
+//
+//  Vertex.swift
+//  LidarTest
+//
+//  Created by Алексей Ведушев on 02.04.2021.
+//
+
+import Foundation
+import simd
+
+
+struct Vertex {
+    let position: vector_float3
+    let normal: vector_float3
+    let uv0: vector_float2
+}
